26c1e03990d1f9898970f04912a6373ce3347258
ajtoyoda <ajtoyoda@ucalgary.ca>
2014-11-29 15:38:32 -0700

    Jamie commiting index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+  require_once("/php/user-permissions.php");
+  require_once("/php/login-functions.php");
+  require_once("/php/index-functions.php");	
+  verifyLoggedIn();
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="robots" content="noindex, nofollow" />
+    <title>Home :: Medical Research Database</title>
+    
+    <link rel="stylesheet" href="/css/layout.css" />
+    <link rel="stylesheet" href="/css/nav.css" />
+    <link rel="stylesheet" href="/css/info-box.css" />
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div id="logout-tab">
+          <p><a href="/logout.php">Log out</a></p>
+        </div>
+        <div class="padding">
+          <h1><a href="/" title="Home">Medical Research Database</a></h1>
+        </div>
+      </div>
+    </header>
+    <nav>
+      <div class="container">
+        <ul>
+          <li id="current"><a href="/">My studies</a></li>
+          <li><a href="/patients.php">Patients</a></li>
+          <?php printManageUsers(false); ?>
+          <li><a href="/account.php">My account</a></li>
+        </ul>
+        <div class="clearfix"></div>
+      </div>
+    </nav>
+    <div id="content-outer">
+      <div class="container">
+        <div id="content-inner">
+          <div class="padding">
+            <div class="info-box">
+              <ul>
+					<h1>My studies</h1>
+					<?php
+						$canViewStudies = getStudies();
+						for($count = 0; $count < count($canViewStudies); $count++){
+						  showStudy($canViewStudies[$count]);
+						}
+					?>
+              </ul>
+              <div class="clearfix" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <div class="container">
+        <div class="padding">
+          <p><a href="/">My studies</a> | <a href="/patients.php">Patients</a> | <?php printManageUsers(true); ?> <a href="/account.php">My account</a></p>
+          <p>Copyright &copy; 2014. Use of this website is permitted for authorized personnel only.</p>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
